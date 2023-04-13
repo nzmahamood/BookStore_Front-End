1726a33358e1443409740f606e05fdeaae1be722
nzmahamood <nmmundekkat@uclan.ac.uk>
2023-04-13 20:45:17 +0100

    added basket api change3

--- src/contexts/store/basketapi.js
+++ src/contexts/store/basketapi.js
@@ -1,15 +1,8 @@
 import axios from "axios"
 import { BASE_URL_NET } from "../../utils/domains"
-import { useSelector } from "react-redux"
-import React from "react";
 
-const UseAccessToken = () => {
-    const accessToken = useSelector((state) => state.token.access_token);
-    return accessToken;
-  };
 
 const add_to_cart = (data, access) => {
-    const access_token = UseAccessToken()
     axios.post(`${BASE_URL_NET}/basket/api/add_to_basket/`, data, {
         headers:{
             "Content-Type": "application/json",
